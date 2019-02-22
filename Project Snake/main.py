8f080f6e9d41006fe80478e41f9579aa199e452d
alexan2dre123 <34386707+alexan2dre123@users.noreply.github.com>
2019-02-21 22:44:02 -0300

    Update main.py

--- Project Snake/main.py
+++ Project Snake/main.py
@@ -1,10 +1,16 @@
 from Lib.functions import *
+import random
 import os
 
 jogador = obj(1, 1, 'o')
+fruta = obj(random.randint(0,LARGURA),random.randint(0,ALTURA), '*')
 
 while (True):
-    mapa(jogador, jogador)
+    if jogador.x == fruta.x and jogador.y == fruta.y:
+        fruta.x = random.randint(0,LARGURA)
+        fruta.y = random.randint(0,ALTURA)
+
+    mapa(jogador, fruta)
     keyboard_setup(jogador)
     sleep(0.2)
     os.system('cls')
